947e54f74df83fa0c9c7f2c04380c04aed2c67ca
Mustafa-Ki-O <robnsonmark11@gmail.com>
2025-09-12 17:15:54 +0300

    some update-PWA final 3.6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/National_Diabetes_Program/'
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB بدلاً من 2MB
+      },
+      manifest: {
+        name: 'National Diabetes Program',
+        short_name: 'Diabetes Program',
+        description: 'A Progressive Web App for Diabetes Management',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/public/icons/ndblogo2.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/public/icons/ndblogo2.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/National_Diabetes_Program/',
 })
